bff830924a2d71b7a6a81d8a4d277fee56d5a379
Patel Gaurang <82432546+gaurangpatel-1@users.noreply.github.com>
2022-01-16 11:19:47 +0530

    Add files via upload

---
+++ gaurangpatel-1/Dictionary/3.py
@@ -0,0 +1,9 @@
+dict1 = {
+    "toys":150,
+    "apple":100,
+    "grapes":135,
+    "tomato":95,
+    "onion":80
+    
+}
+print(sum(dict1.values()))
---
+++ gaurangpatel-1/Dictionary/4.py
@@ -0,0 +1,13 @@
+dictionary = {
+    "name":"Gaurang",
+    "age":"19",
+    "gender":"Male"
+}
+
+print(dictionary)
+
+weight = input("Enter your weight: ")
+
+dictionary["weight"] = weight
+
+print(dictionary)
