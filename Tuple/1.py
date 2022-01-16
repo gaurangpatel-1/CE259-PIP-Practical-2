809840872b0f7c9fd789e75bff72b9aa326c6b88
Patel Gaurang <82432546+gaurangpatel-1@users.noreply.github.com>
2022-01-16 11:21:42 +0530

    Create 1.py

---
+++ Tuple/1.py
@@ -0,0 +1,13 @@
+a = 81
+b = "My Name Is Gaurang"
+c = ["Hello","Python","World"]
+d= {
+    "name":"Gaurang",
+    "age":"19",
+    "roll_no":"20CE081",
+    "collage" : "CHARUSAT"
+    }
+
+tuple = (a,b,c,d)
+
+print(tuple)
